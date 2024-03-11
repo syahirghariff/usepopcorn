8fe967aeae98900e10c325240745db8dcec257ba
syahirghariff <syahirghariff@gmail.com>
2024-03-11 11:43:32 +0800

    Section 10-112: fixing prop drolling with compostion

--- src/App.js
+++ src/App.js
@@ -47,12 +47,11 @@ const tempWatchedData = [
   },
 ];
 
-function NavBar({movies}) {
+function NavBar({children}) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      {children}
     </nav>
   );
 }
@@ -88,11 +87,10 @@ function Search() {
   );
 }
 
-function Main({ movies }) {
+function Main({ children }) {
   return (
     <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
+      {children}
     </main>
   );
 }
@@ -119,7 +117,7 @@ function WatchedBox() {
   );
 }
 
-function ListBox({movies}) {
+function ListBox({children}) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -130,7 +128,7 @@ function ListBox({movies}) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
@@ -232,8 +230,16 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
