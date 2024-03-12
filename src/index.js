a54ea19d9a7e92718f91981244c6781de0306d23
syahirghariff <syahirghariff@gmail.com>
2024-03-12 09:56:42 +0800

    Section 10-119: improving reusability with props

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating maxRating={5} messages={['Terrbile', 'Bad', 'Okay', 'Good', 'Amazing']} />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
     <App />
   </React.StrictMode>
 );
