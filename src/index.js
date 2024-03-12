c93f76effeff8b178adb077d87f062318540ebd6
syahirghariff <syahirghariff@gmail.com>
2024-03-12 09:34:31 +0800

    Section 10-117: creating rating star

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import StarRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <StarRating maxRating={5} />
     <App />
   </React.StrictMode>
 );
