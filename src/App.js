05ddbe8d1dd3b729654f42757631a3648f2a011f
syahirghariff <32362280+syahirghariff@users.noreply.github.com>
2024-03-22 02:08:53 +0000

    Section 12-151: loading movie details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import StarRating from "./StarRating";
 
 const tempMovieData = [
   {
@@ -136,12 +137,63 @@ function Movie({ movie, onSelectMovie }) {
 }
 
 function MovieDetails({selectedId, onCloseMovie}){
-  return <div className="details">
-    <button className="btn-back" onClick={onCloseMovie}>
-      &larr; 
-    </button>
-    {selectedId}
-    </div>
+
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime, 
+    imdbRating, 
+    Plot: plot, 
+    Released: released, 
+    Actors: actors, 
+    Director: director,
+    Genre: genre } = movie;
+
+  console.log(title, year);
+
+  useEffect(()=> {
+    async function getMovieDetails(){
+      setIsLoading(true);
+      const res = await fetch(`https://www.omdbapi.com/?i=${selectedId}&apiKey=${API_KEY}`);
+      const data = await res.json(); 
+      setMovie(data);
+      setIsLoading(false);
+    }
+    getMovieDetails();
+  }, [selectedId])
+
+  return (
+  <div className="details">
+    { isLoading ? <Loader /> : (<>
+    <header>
+      <button className="btn-back" onClick={onCloseMovie}>
+        &larr; 
+      </button>
+      <img src={poster} alt={`Poster of ${movie} movie`} />
+      <div className="details-overview">
+        <h2>{title}</h2>
+        <p>{released} &bull; {runtime} </p>
+        <p> {genre} </p>
+        <p><span>*</span> {imdbRating} IMDb rating</p>
+      </div>
+    </header>
+
+    <section>
+      <div className="rating">
+        <StarRating maxRating={5} size={24} />
+      </div>
+      
+      <p><em>{plot} </em></p>
+      <p>Starring {actors} </p>
+      <p> Directed by {director} </p>
+    </section>
+  </>)}
+    
+    
+  </div>)
 }
 
 function WatchedSummary({ watched }) {
@@ -267,7 +319,7 @@ export default function App() {
       try {
         setIsLoading(true);
         setError('');
-        const res = await fetch(`https://www.omdbapi.com/?i=${I_KEY}&apiKey=${API_KEY}&s=${query}`);
+        const res = await fetch(`https://www.omdbapi.com/?apiKey=${API_KEY}&s=${query}`);
         if (!res.ok) {
           throw new Error ("Something went wrong with fetching movies");
         }
